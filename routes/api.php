4d504d086c87312cdc60616c533a4e81e176b53d
sahimjbc <jbc.sahim@gmail.com>
2025-11-03 14:38:56 +0600

    refactor: Remove redundant line in API route definition for cleaner code

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,6 @@ $router->group(['prefix' => 'v1'], function ($router) {
 
 $router->group(['prefix' => 'v1'], function ($router) {
     $router->group(['prefix' => 'v2', 'as' => 'posts'], function ($router) {
-
         $router->get('/posts/{id}', [PostController::class, 'show'])->name('index');
     });
 });
